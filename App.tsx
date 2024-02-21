76411aba71843fe2558c89f2cd487568aec0dde1
Alvaro Bautista <67112345+alvrba@users.noreply.github.com>
2024-02-21 09:42:23 +0100

    fix: improve initial loading screen (#172)
    * feat: splash screen including react navigation loading
    
    * fix: change expo-splash-screen version
    
    * fix: add comments and catch notation
    
    * fix: update packages
    
    * fix: update packages

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { I18nextProvider } from 'react-i18next';
 import { LogBox } from 'react-native';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
 // polyfill the URL and URLParams apis
 import 'react-native-url-polyfill/auto';
 
@@ -17,6 +18,9 @@ import AppNavigator from './src/navigation/AppNavigator';
 LogBox.ignoreAllLogs();
 
 function App() {
+  // Prevent hidding spash screen until real content is available. Disabled in RootNavigator
+  SplashScreen.preventAutoHideAsync();
+
   return (
     <AuthProvider>
       <I18nextProvider i18n={i18nConfig}>
