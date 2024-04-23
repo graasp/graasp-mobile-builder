4d5157ab821522b8b06aab962e5fa3c5913f72ce
kim <kim.phanhoang@epfl.ch>
2024-04-23 09:47:26 +0200

    ci: allow dispatch for ios build

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
-import * as Sentry from '@sentry/react-native';
 import { I18nextProvider } from 'react-i18next';
 import { LogBox } from 'react-native';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as SplashScreen from 'expo-splash-screen';
 // polyfill the URL and URLParams apis
 import 'react-native-url-polyfill/auto';
 
+import * as SplashScreen from 'expo-splash-screen';
+
+import * as Sentry from '@sentry/react-native';
 // polyfill for android intl
 import 'intl';
-import 'intl/locale-data/jsonp/en';
 import 'intl-pluralrules';
+import 'intl/locale-data/jsonp/en';
 
 import i18nConfig from './src/config/i18n';
 import { AuthProvider } from './src/context/AuthContext';
@@ -20,7 +21,7 @@ import AppNavigator from './src/navigation/AppNavigator';
 LogBox.ignoreAllLogs();
 
 Sentry.init({
-  dsn: process.env.EXPO_PUBLIC_SENTRY_DNS,
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
   debug: false,
 });
